4e3fbbcc76112ed6c06e3b966c877a06dbcc41f0
조규용 <mvpcouple@naver.com>
2024-03-03 11:33:42 +0900

    solution reg back 성공

--- api/controllers/solution.js
+++ api/controllers/solution.js
@@ -9,11 +9,18 @@ export const register = (req, res) => {
     if (err) return res.status(500).json(err);
     if (data.length) return res.status(409).json("Solution 과제가 이미 있습니다!");
 
-    const insertQuery = "INSERT INTO solutiondata (`sol_name`, `kor_name`, `n_id`) VALUES (?, ?, ?)";
+    const insertQuery = "INSERT INTO solutiondata (`sol_name`, `sol_full_name`, `kor_name`,`n_id`, `url`, `github_url`,`work_field`, `date`, `likeCnt`,`img`) VALUES (?, ?, ?,?, ?, ?,?, ?, ?,?)";
     const values = [
       req.body.sol_name,
+      req.body.sol_full_name,
       req.body.kor_name,
       req.body.n_id,
+      req.body.url,
+      req.body.github_url,
+      req.body.work_field,
+      req.body.date,
+      req.body.likeCnt,
+      req.body.img
     ];
 
     db.query(insertQuery, values, (err, data) => {
